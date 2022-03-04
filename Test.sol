157e4fc4cb21aa16d3c4f19fffbce67cc4926e5b
Mark Tyneway <mark.tyneway@gmail.com>
2022-03-04 08:46:27 -0800

    fix formatting

--- Test.sol
+++ Test.sol
@@ -4,10 +4,10 @@ pragma solidity >=0.8.11;
 contract Test {
 	function bar(uint256 frob) public pure returns (uint256) {
         uint256 baz;
-		assembly {
-			tstore(0, frob)
+        assembly {
+            tstore(0, frob)
             baz := tload(0)
 		}
         return baz;
-	}
+    }
 }
